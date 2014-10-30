993bc9b34d5960f207c0ab82363caf899065fede
Matt Bajor <mbajor@rallydev.com>
2014-10-30 16:43:54 -0600

    More test jobs

---
+++ plan_b/job-2.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+  10.100.199.201:8080/v2/apps -d '
+{
+    "id": "www2",
+    "container": {
+        "docker": {
+            "image": "mbajor/webapp",
+            "network": "BRIDGE",
+            "portMappings": [
+                { "containerPort": 80, "hostPort": 0, "servicePort": 9000, "protocol": "tcp" }
+            ]
+        },
+        "type": "DOCKER",
+        "volumes": []
+    },
+    "cpus": 1,
+    "mem": 256,
+    "instances": 1
+}'
